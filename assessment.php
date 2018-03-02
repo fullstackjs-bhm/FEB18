d701a25950b4f6d80a77e304da0566d111903bef
devBustamento <dev.bustamento@gmail.com>
2018-03-01 20:02:24 -0600

    last touches for now

--- assessment.php
+++ assessment.php
@@ -1,24 +1,26 @@
 <?php require_once('includes/header-no-menu.php'); ?>
 
-<div class="">
-        <div class="row">
+
+        <div class="">
             <div id="testAreaContent" class="col-12 col-md-8 mx-auto my-3">
-                <p class="my-3" id="testQuestion">1. I have a question, are your shoes tied?</p>
-                <div class="ansContainer push-md-2 my-2">
+                <p class="my-3 text-center" id="testQuestion">1. I have a question, are your shoes tied?</p>
+                <div class="ansContainer my-1">
                     <input type="radio" name="choice" value="a" /> <span id="optionA">Answer A</span>
                 </div>
-                <div class="ansContainer my-2">
-                    <input type="radio" name="choice" value="b" />  <span id="optionB">Answer B</span>
+                <div class="ansContainer my-1">
+                    <input class="btn-group-vertical" type="radio" name="choice" value="b" />  <span id="optionB">Answer B</span>
                 </div>
-                <div class="ansContainer my-2">
-                    <input type="radio" name="choice" value="c" />  <span id="optionC">Answer C</span>
+                <div class="ansContainer my-1">
+                    <input class="btn-group-vertical" type="radio" name="choice" value="c" />  <span id="optionC">Answer C</span>
                 </div>
-                <div class="ansContainer my-2">
-                    <input type="radio" name="choice" value="d" />  <span id="optionD">Answer D</span>
+                <div class="ansContainer my-1">
+                    <input class="btn-group-vertical" type="radio" name="choice" value="d" />  <span id="optionD">Answer D</span>
                 </div>
                 <div class="text-center">
                     <button type="button" id="nextBTN" class="btn btn-success">Next</button>
                 </div>
             </div>
-        </div>
+        
     </div>
+
+<?php require_once('includes/ez-footer.php'); ?>
---
+++ includes/ez-footer.php
@@ -0,0 +1,31 @@
+    <!-- Footer -->
+      <div class="ez-footer container-fluid footer">
+        <div class="container">
+          
+
+          <div class="row py-3 copyTerms">
+            <div class="col-12 col-md-6 text-center text-md-left">&copy; Copyright <?php echo date('Y'); ?> Full Stack BHM</div>
+            <div class="col-12 col-md-6 text-center text-md-right"><a class="mx-1" href="#">Terms &amp; Conditions</a> <span style="color: #fff;">|</span> <a class="mx-1" href="#">Privacy Policy</a></div>
+          </div>
+      </div>
+    </div>
+    
+
+<!--  *** End of page scripts ***  -->
+    <!-- Current jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+      window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+      ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+
+    <!-- navMenu Script -->
+    <script type="text/javascript" src="js/navMenu.js"></script>
+    <script src="scripts/form.js"></script>
+    <script src="js/question.js"></script>
+    
+  </body>
+</html>
